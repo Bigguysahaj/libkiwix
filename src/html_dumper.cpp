03430917f664801ab2084e6914376183edb9cdc9
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2023-02-16 02:29:18 +0530

    Add Results label
    Shows "x results" label where x = number of books based on filters

--- src/html_dumper.cpp
+++ src/html_dumper.cpp
@@ -65,7 +65,8 @@ std::string HTMLDumper::dumpPlainHTML(kiwix::Filter filter) const
              kainjow::mustache::object{
                {"root", rootLocation},
                {"books", booksData },
-               {"searchQuery", searchQuery}
+               {"searchQuery", searchQuery},
+               {"resultsCount", to_string(filteredBooks.size())}
              }
   );
 }
