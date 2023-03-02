1cd8ca4a7ebe98f8277bfa16159ff898d7abe5a9
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2023-03-03 00:40:04 +0530

    Introduce HTMLDumper
    HTMLDumper class will be used to dump library in HTML format. It inherits from LibraryDumper

---
+++ include/html_dumper.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Nikhil Tanwar <2002nikhiltanwar@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU  General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef KIWIX_HTML_DUMPER_H
+#define KIWIX_HTML_DUMPER_H
+
+#include <string>
+
+#include "library_dumper.h"
+#include "library.h"
+#include "name_mapper.h"
+
+
+namespace kiwix
+{
+
+/**
+ * A base class to dump Library in HTML format.
+ */
+class HTMLDumper : public LibraryDumper
+{
+ public:
+  HTMLDumper(const Library* const library, const NameMapper* const NameMapper);
+  ~HTMLDumper();
+
+
+  /**
+   * Dump library in HTML
+   *
+   * @return HTML content
+   */
+  std::string dumpPlainHTML() const;
+};
+
+}
+
+#endif // KIWIX_HTML_DUMPER_H
